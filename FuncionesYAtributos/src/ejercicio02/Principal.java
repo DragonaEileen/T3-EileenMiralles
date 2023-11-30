a3a6986f883a715e9c78a3de53e6e478df1fda41
DragonaEileen <eileen.miralles@iesnervion.es>
2023-11-30 14:41:43 +0100

    Ejercicio 2 Completado

--- FuncionesYAtributos/src/ejercicio02/Principal.java
+++ FuncionesYAtributos/src/ejercicio02/Principal.java
@@ -1,10 +1,166 @@
 package ejercicio02;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Principal {
+	
+	/* Pruebas */
+	/* 	Entrada: 1, 1, 1 		 | S. Esperada: 4			|S. Obtenida: 4
+	 * 	Entrada: 1, 1, 2		 | S. Esperada: 1			|S. Obtenida: 1
+	 *  Entrada: hola, hola, hola| S. Esperada: Exceptions	|S. Obtenida: Exceptions
+	 * */
+	
+	/* Apertura de Scanner Estático */
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+		/* Declaraciones */
+			/* Variables */
+			/* Declaramos un entero para guardar la opción del menu;
+			 * Y declaramos dos dobles para guardar el ancho y el alto */
+		int menu;
+		double alto, ancho;
+		
+			/* Objetos */
+			/* Declaramos el Objeto de clase Rectangulo */
+		Rectangulo rectangle;
+		
+		/* Algoritmo */
+		/* Primero pedimos el ancho y el alto */
+		ancho = pideAncho();
+		alto = pideAlto();
+		
+		/* Luego mostramos el menu y pedimos la opción */
+		menu = menu();
+		
+		/* Ahora Construimos con el ancho y el alto el rectangulo */
+		rectangle = new Rectangulo(alto, ancho);
+		
+		/* Según la opción llamamos a una función u otra */
+		switch (menu) {
+		case 1: 
+			
+			System.out.println("El perímetro es " + rectangle.perimetro());
+			break;
+			
+		case 2:
+			
+			System.out.println("El area es " + rectangle.area());
+			break;
+			
+		case 0:
+			
+			System.out.println("Bye Bye");
+			break;
+			
+		default:
+			
+			throw new IllegalArgumentException("Unexpected value: " + menu);
+			
+		}//Fin Switch --> Options
+		
+		/* Cierre de Scanner */
+		sc.close();
+		
+	}//FIN MAIN
+	
+	/* Métodos */
+		/* Menú */
+		/* Mostrará las opciones y pedirá un numero entero que devolverá */
+	public static int menu() {
+		
+		/* Declaración de Variables */
+		int menu = -1;
+		
+		/* Presentación de Opciones */
+		System.out.println("1. Perímetro\r\n"
+				+ "2. Área\r\n"
+				+ "0. Salir\r\n");
+		
+		/* Leemos el dato a introducir, un dato entero entre 0 y 2 */
+		do{
+			
+			try{
+				
+				System.out.print("Introduce la Opción: ");
+				menu = sc.nextInt();
+				
+			}catch(InputMismatchException e){
+				
+				System.err.println("Dato no válido.");
+				sc.nextLine();
+				
+			}//Fin Try-Catch
+			
+		}while(menu > 2 || menu < 0);
+		
+		/* Devolvemos Variable */
+		return menu;
+		
+	}//Fin menu() method
+	
+		/* Ancho */
+		/* Pide que se introduzca el ancho (double), comprueba que es correcto, y lo devuelve */
+	public static double pideAncho() {
+		
+		/* Declaración de Variables */
+		/* Declaramos el Ancho como un Doble */
+		double ancho = 0;
+		
+		/* Recogida de Datos */
+		/* Con un Do-Try nos aseguramos que es un número y mayor que 0 */
+		do{
+			
+			try{
+				
+				System.out.print("Introduce el Ancho: ");
+				ancho = sc.nextDouble();
+				
+			}catch(InputMismatchException e){
+				
+				System.err.println("Wrong Data. Try Again.");
+				sc.nextLine();
+				
+			}//Fin Try-Catch
+			
+		}while(ancho <= 0);
+		
+		/* Return */
+		return ancho;
+		
+	}//Fin pideAncho()
+	
+		/* Altura */
+		/* Pedimos la altura, lo comprobamos, y lo devolvemos */
+	public static double pideAlto() {
+		
+		/* Declaración de Variables */
+		/* Declaramos un doblem Alto */
+		double alto = 0;
+		
+		/* Recogida de Datos */
+		/* Para comprobar que es un número y es mayor a 0, usamos un Do-Try */
+		do{
+			
+			try{
+				
+				System.out.print("Introduce la altura: ");
+				alto = sc.nextDouble();
+				
+			}catch(InputMismatchException e){
+				
+				System.err.println("Wrong data type. Try Again.");
+				sc.nextLine();
+				
+			}//Fin Try-Catch
+			
+		}while(alto <= 0);
+		
+		/* Return */
+		return alto;
 
+	}//Fin pideAlto()
+	
 }
--- FuncionesYAtributos/src/ejercicio02/Rectangulo.java
+++ FuncionesYAtributos/src/ejercicio02/Rectangulo.java
@@ -2,4 +2,86 @@ package ejercicio02;
 
 public class Rectangulo {
 
+	/* Declaración de Atributos */
+	/* Declaramos el ancho y el alto del rectangulo, van a ser dobles para soportar decimales */
+	private double alto;
+	private double ancho;
+	
+	/* Constructores */
+		/* Constructor vacío, con default values de 10 */
+	public Rectangulo() {
+		
+		alto = 10;
+		ancho = 10; 
+		
+	}//Fin Constructor Vacío
+	
+		/* Constructor con parámetros, alto y ancho serán igual a los parámetros introducidos */
+	public Rectangulo(double alto, double ancho) {
+		
+		this.alto = alto;
+		this.ancho = ancho;
+		
+	}
+	
+	/* Getters and Setters */
+		/* Getters */
+	public double getAlto() {
+		
+		return alto;
+		
+	}
+	
+	public double getAncho() {
+		
+		return ancho;
+		
+	}
+	
+		/* Setters */
+	public void setAlto(int alto) {
+		
+		this.alto = alto;
+		
+	}
+	
+	public void setAncho(int ancho) {
+		
+		this.ancho = ancho;
+		
+	}
+	
+	/* Methods */
+		/* Perímetro */
+		/* Devolvemos la suma de dos anchos y dos altos */
+	public double perimetro() {
+		
+		/* Declaración de Variables */
+		/* Let's declare a double that keeps the perimeter for the code's ease */
+		double perimetro = 0;
+		
+		/* Calculi */
+		perimetro = (ancho*2) + (alto*2);
+	 	
+		/* Return */
+		return perimetro;
+		
+	}//Fin perimetro()
+	
+		/* Area */
+		/* Devolvemos el ancho por la altura */
+	public double area() {
+		
+		/* Declaración de Variables */
+		/* Declaramos una variable area por claridad del código */
+		double area = 0;
+		
+		/* Calculi */
+		area = ancho * alto;
+		
+		/* Return */
+		return area;
+		
+	}
+	
 }
