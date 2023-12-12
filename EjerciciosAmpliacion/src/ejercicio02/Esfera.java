fda9e6be779b9b95b2470770de5028ae59e59b53
Cyber Dragons <elemento.gemini@gmail.com>
2023-12-12 19:50:23 +0100

    Ejercicio 2 Terminado

--- EjerciciosAmpliacion/src/ejercicio02/Esfera.java
+++ EjerciciosAmpliacion/src/ejercicio02/Esfera.java
@@ -37,5 +37,36 @@ public class Esfera {
 	}//Fin setRadio()
 	
 	/* Methods */
+		/* Superficie */
+		/* La superficie de una esfera se calcula con el radio, devolveremos un double */
+	public double mathSurface() {
+		
+		/* Declaración de Variables */
+		/* Por claridad del código vamos a declarar una variable surface para devolver */
+		double surface;
+		
+		/* Algorithm */
+		surface = 4 * Math.PI * Math.pow(radio, 2);
+		
+		/* Return */
+		return surface;
+		
+	}// Fin mathSurface()
+	
+		/* Volumen */
+		/* Esta función devuelve el double del volumen calculado a partir del radio */
+	public double mathVolume() {
+		
+		/* Declaración de Variables */
+		/* Por claridad del código vamos a declarar una variable volume para devolver */
+		double volume;
+		
+		/* Algoritmo */
+		volume = (4 * Math.PI / 3 )* Math.pow(radio, 3);
+		
+		/* Return */
+		return volume;
+		
+	}// Fin mathVolume()
 
 }
--- EjerciciosAmpliacion/src/ejercicio02/Principal.java
+++ EjerciciosAmpliacion/src/ejercicio02/Principal.java
@@ -1,18 +1,61 @@
 package ejercicio02;
 
+import java.util.Scanner;
+
 public class Principal {
 	
 	/* Pruebas */
 	/*	Entrada: 0 		| S. Esperada: Try Again	|S. Obtenida: Try Again
-	 * 	Entrada: 11 	| S. Esperada: true, true	|S. Obtenida: true true
-	 * 	Entrada: 97		| S. Esperada: true, false	|S. Obtenida: true false
-	 * 	Entrada: 22		| S. Esperada: false, true	|S. Obtenida: false true
-	 *  Entrada: 14		| S. Esperada: false, false	|S. Obtenida: false false
+	 * 	Entrada: 10 	| S. Esperada: 1256, 4188	|S. Obtenida: true true
 	 *  Entrada: hola	| S. Esperada: Exception	|S. Obtenida: Exception
 	 * */
 
 	public static void main(String[] args) {
 
+		/* Declaraciones */
+			/* Variables */
+			/* Declaramos una variable para el dato introducido radio;
+			 * luego dos variables para el volumen y la superficie */
+		double radio = 0, surface, volume;
+		
+			/* Objetos */
+		Esfera sphere;
+		
+		/* Apertura de Scanner */
+		Scanner sc = new Scanner(System.in);
+		
+		/* Recogida de Datos */
+		do{
+			
+			try{
+				
+				System.out.print("Introduce el radio: ");
+				radio = sc.nextDouble();
+				
+			}catch(Exception e){
+				
+				System.err.println("Wrong Data Type. Try Again.");
+				sc.nextLine();
+				
+			}//Fin Try-Catch
+			
+		}while(radio <= 0);
+		
+		/* Algoritmo */
+		/* Construimos el objeto sphere */
+		sphere = new Esfera(radio);
+		
+		/* Llamamos a las funciones */
+		surface = sphere.mathSurface();
+		volume = sphere.mathVolume();
+		
+		/* Respuesta */
+		System.out.println("La superficie es de: " + surface);
+		System.out.println("El volumen es de: " + volume);
+		
+		/* Cierre de Scanner */
+		sc.close();
+		
 	}//FIN MAIN
 
 }
